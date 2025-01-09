799a0d7238c7ae4b7769f46d9e9a4a6ef8a35297
reebxu459 <rebbi.xu123@gmail.com>
2025-01-09 01:23:36 -0500

    endpoint draft for create_orderbook

--- projects/orderbook/app.py
+++ projects/orderbook/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, jsonify, request
 from db_config import get_db_connection
+from docker_utils import build_docker_image, run_docker_container, stop_docker_container
+
+ORDERBOOKS_TABLE_NAME = 'order_books'
 
 
 app = Flask(__name__)
@@ -20,23 +23,45 @@ def create_orderbook():
     tickers_to_track = request.args.get('tickerstotrack', '').split(',')
     algo_path = request.args.get('algo_path')
     update_time = request.args.get('updatetime', type=int)
-    end = request.args.get('end', type=int)
+    end_duration = request.args.get('end', type=int)
 
     # input validation
-    if not name or not algo_path or not update_time or not end:
+    if not name or not algo_path or not update_time or not end_duration:
         return jsonify({"error": "Missing required parameters"}), 400
     
-    # paths to pull algorithm and store image
-    path_to_algo = f"../{algo_path}"
-    path_to_image = f"docker_images/{name}.tar"
-    
-    # check if image with this name already exists
-    if os.path.exists(path_to_image):
-        return jsonify({'Error': "You've already built this image"}), 400
-    
-    # build docker image
-    image = build_docker_image()
+    try:
+        # paths to pull algorithm and store image
+        path_to_algo = f"../{algo_path}"
+        path_to_image = f"docker_images/{name}.tar"
+        
+        # check if image with this name already exists. If not, build it from the path_to_algo.
+        if not os.path.exists(path_to_image):
+            # build docker image
+            image = build_docker_image(name, path_to_algo)
 
+            # save image as .tar to path_to_image
+            with open(path_to_image, 'wb') as image_tar:
+                for chunk in image.save():
+                    image_tar.write(chunk)
+            print(f'Saved Docker image for {name} to {path_to_image}')
+
+        # save the order book in the database
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute(
+            f"""
+                INSERT INTO {ORDERBOOKS_TABLE_NAME} (name, tickers_to_track, algo_link, update_time, end_duration)
+                VALUES (%s, %s, %s, %s, %s)
+            """, (name, tickers_to_track, algo_path, update_time, end_duration)
+        )
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return jsonify({'info': f"Order book '{name}' has been created."}), 201
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 
