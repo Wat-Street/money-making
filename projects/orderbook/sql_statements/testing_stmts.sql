46bb69f76ca1224163b1a2a2376675798dddb000
reebxu459 <rebbi.xu123@gmail.com>
2025-01-11 00:57:35 -0500

    prepared dummy model directory for endpoint testing

--- projects/orderbook/sql_statements/testing_stmts.sql
+++ projects/orderbook/sql_statements/testing_stmts.sql
@@ -6,3 +6,22 @@ VALUES (
     '{15, 19, 40}',
     78549
 );
+
+
+-- example entry in order_books_v2
+INSERT INTO order_books_v2 (name, tickers_to_track, algo_link, update_time, end_duration)
+VALUES ('test', '{"AAPL","GOOG"}', 'hi', '1', '100');
+
+
+-- table creation
+CREATE TABLE order_books_v2 (
+    name TEXT PRIMARY KEY,
+    tickers_to_track TEXT[],
+    algo_link TEXT NOT NULL,
+    update_time INT NOT NULL,
+    end_duration INT NOT NULL,
+    trades JSONB DEFAULT '[]',
+    worth NUMERIC[] DEFAULT '{}',
+    balance NUMERIC NOT NULL DEFAULT 100000,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
